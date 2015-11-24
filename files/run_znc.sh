6dcaba313aefe2bfdb62d7bb89f6a9e5156c4651
echel0n <echel0n@users.noreply.github.com>
2015-11-24 03:08:19 -0800

    Update run_znc.sh

--- files/run_znc.sh
+++ files/run_znc.sh
@@ -1,15 +1,5 @@
 set -e
 
 echo "starting znc on port ($PORT)"
-
 sed "s/HEROKUPORT/$PORT/" /app/.znc/configs/znc.conf
-./znc/bin/znc -f &
-export ZNCPID=$!
-echo "waiting for znc ($ZNCPID) to exit......."
-while [ -e /proc/$ZNCPID ]
-do
-    echo "$ZNCPID is running"
-    sleep 600
-done
-
-kill $ZNCPID
+./znc/bin/znc -fD
